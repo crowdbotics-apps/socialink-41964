502d06d3d46fc8cad071d997085180d80dd9f667
Crowdbotics <team@crowdbotics.com>
2023-07-07 03:03:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "socialink-41964",
+  url: "https://socialink-41964.botics.co",
+  api: "https://socialink-41964.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
